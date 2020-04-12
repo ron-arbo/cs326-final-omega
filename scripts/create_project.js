b63e1f32cb44eff8ca4c8cd6af9d618c39deb198
aryan singh <aryansingh@umass.edu>
2020-04-11 21:43:06 -0400

    some changes

---
+++ scripts/create_project.js
@@ -0,0 +1,7 @@
+
+let project_name = document.getElementById('exampleFormControlInput1').value;
+let project_description = document.getElementById('exampleFormControlTextarea1').value;
+let developers_working = document.getElementById('exampleFormControlTextarea2').value;
+let duration = document.getElementById('exampleFormControlTextarea4').value;
+let helpful_links = document.getElementById('exampleFormControlTextarea5').value;
+let developers_needed = document.getElementById('exampleFormControlInput2').value;
