2cd45962533a2add46519e09ca67ab7d730983cb
aryan singh <aryansingh@umass.edu>
2020-04-14 16:56:21 -0400

    mongo template

---
+++ backend/mongo-database.ts
@@ -0,0 +1,89 @@
+export class Database {
+
+    private MongoClient = require('mongodb').MongoClient;
+    private uri = "mongodb+srv://guest:guest@cluster0-y0tyl.mongodb.net/test?retryWrites=true&w=majority";
+    private client;
+    private collectionName: string; // was = "books";
+    private dbName: string = "emery";
+
+    constructor(collectionName) {
+        this.collectionName = collectionName;
+        this.client = new this.MongoClient(this.uri, { useNewUrlParser: true });
+        // Open up a connection to the client.
+        // The connection is asynchronous, but we can't call await directly
+        // in the constructor, which cannot be async. So, we use "IIFE". Explanation below.
+
+        /* from https://anthonychu.ca/post/async-await-typescript-nodejs/
+    ​
+          Async/Await and the Async IIFE
+    ​
+          The await keyword can only be used inside of a function
+          marked with the async keyword. [...] One way to do this is
+          with an "async IIFE" (immediately invoked function
+          expression)...
+    ​
+           (async () => {
+           code goes here
+           })();
+    ​
+        */
+        (async () => {
+            await this.client.connect().catch(err => { console.log(err); });
+        })();
+    }
+
+    public async put(key: string, value: string): Promise<void> {
+        console.log("put: key = " + key + ", value = " + value);
+        let db = this.client.db(this.dbName);
+        let collection = db.collection(this.collectionName);
+        let result = await collection.updateOne({ 'name': key }, { $set: { 'value': value } }, { 'upsert': true });
+        console.log("result = " + result);
+    }
+
+    public async get(key: string): Promise<string> {
+        // YOUR CODE GOES HERE
+        let db = this.client.db(this.dbName);
+        let collection = db.collection(this.collectionName);
+        let result = await collection.findOne({ 'name': key });
+
+        console.log("get: returned " + JSON.stringify(result));
+        if (result) {
+            return result.value;
+        } else {
+            return null;
+        }
+
+    }
+
+    public async del(key: string): Promise<void> {
+
+        console.log("delete: key = " + key);
+        // YOUR CODE GOES HERE
+        let db = this.client.db(this.dbName);
+        let collection = db.collection(this.collectionName);
+        await collection.deleteOne({ 'name': key })
+
+    }
+
+    // public async isFound(key: string): Promise<boolean> {
+    //     // console.log("isFound: key = " + key);
+    //     // let v = await this.get(key);
+    //     // console.log("is found result = " + v);
+    //     try {
+    //         let v : string = await this.get(key);
+    //         return true;
+    //     } catch (err) {
+    //         return false;
+    //     }
+    // }
+    public async isFound(key: string): Promise<boolean> {
+        console.log("isFound: key = " + key);
+        let v = await this.get(key);
+        console.log("is found result = " + v);
+        if (v === null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
