888e35bb412a778300e1da8be891c2e5e102588a
aryan singh <aryansingh@umass.edu>
2020-04-16 17:24:00 -0400

    trying to run this on heroku

--- server-main.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Database } from './mongo-database';
-import { MyServer } from './myserver-routing';
-
-const theDatabase = new Database('YOUR-GITHUB-ALIAS'); // CHANGE THIS
-const theServer = new MyServer(theDatabase);
-
-theServer.listen(8080);
--- server.ts
+++
@@ -1,6 +0,0 @@
-let express = require('express');
-let app = express()
-
-express.use(express.static('./'))
-
-app.listen(process.env.PORT || 8080, ()=>console.log('Good work'))
