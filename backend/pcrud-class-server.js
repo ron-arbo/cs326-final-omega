df1292ccc1750c1d11e3d5b05503f29ba8621d22
ron_arbo <ron.arbo63@icloud.com>
2020-04-15 22:06:38 -0400

    Change port number

--- backend/pcrud-class-server.js
+++ backend/pcrud-class-server.js
@@ -4,4 +4,4 @@ var level_database_1 = require("./level-database");
 var myserver_1 = require("./myserver");
 var theDatabase = new level_database_1.Database('counter-db');
 var theServer = new myserver_1.MyServer(theDatabase);
-theServer.listen(8080);
+theServer.listen(process.env.PORT);
